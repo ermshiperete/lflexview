4a9ed50fa9767db249215e7a05e4bbb7eb686d38
Cambell Prince <cambell_prince@sil.org>
2012-06-19 23:19:18 +0700

    Added sample home page

--- src/Look/Default/Views/Home.html.php
+++ src/Look/Default/Views/Home.html.php
@@ -1,1 +1,8 @@
-<p>Some home page</p>
+<h2>Sample Dictionary</h2>
+<p>Enter search term into the search box at the top of the page, then press the 'search' button.</p>
+<p>This dictionary is the Thai Food sample dictionary. Some search terms you can search for are:</p>
+<ul>
+<li>khaw phut muu</li>
+<li>krapâw mǔu</li>
+</ul>
+<p>Note that partial matches are fine.  You don't need to type an exact string. e.g. The IPA in the above example can be quite approximate.</p>
